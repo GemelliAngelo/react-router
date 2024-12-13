e6ac7c053c44fd487f5e3dcb1e342ea7e0bdd5c4
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-13 15:39:00 +0100

    add PostList components

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,71 @@
+import { usePostscontext } from "../contexts/PostsContext";
+import { Link } from "react-router-dom";
+
+export default function PostsList() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  // const [posts, setPosts] = useState([]);
+  const { posts } = usePostscontext();
+
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, []);
+
+  // const fetchPosts = () => {
+  //   fetch(apiUrl + "/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => setPosts(data));
+  // };
+
+  return (
+    <table className="table table-dark table-striped">
+      <thead>
+        <tr className="text-center">
+          <th scope="col">Nome</th>
+          <th scope="col">Anteprima</th>
+          <th scope="col">Categorie</th>
+          <th scope="col">Disponibile</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {posts.map((post) => (
+          <tr key={post.id} className="text-center">
+            <th className="align-middle" scope="row">
+              {post.title}
+            </th>
+            <td>
+              <img src={apiUrl + post.image} width="150" />
+            </td>
+            <td className="align-middle">
+              {post.categories.map((category) => (
+                <span key={category} className="badge text-bg-info ms-2">
+                  {category}
+                </span>
+              ))}
+            </td>
+            <td className="align-middle">
+              {post.published ? (
+                <i className="fa-solid fa-check"></i>
+              ) : (
+                <i className="fa-solid fa-xmark"></i>
+              )}
+            </td>
+            <td className="align-middle text-center">
+              <Link to={`/posts/${post.id}`}>
+                <i
+                  type="button"
+                  className="fa-solid fa-up-right-and-down-left-from-center"
+                ></i>
+              </Link>
+              <i
+                type="button"
+                className="fa-solid fa-trash ms-4 text-danger"
+              ></i>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,77 +1,11 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { usePostscontext } from "../contexts/PostsContext";
+import PostsList from "../components/PostsList";
 
 export default function PostsPage() {
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  // const [posts, setPosts] = useState([]);
-  const { posts } = usePostscontext();
-
-  // useEffect(() => {
-  //   fetchPosts();
-  // }, []);
-
-  // const fetchPosts = () => {
-  //   fetch(apiUrl + "/posts")
-  //     .then((res) => res.json())
-  //     .then((data) => setPosts(data));
-  // };
-
   return (
-    <>
-      <div className="container">
-        <h1 className="my-4">Posts</h1>
-        <table className="table table-dark table-striped">
-          <thead>
-            <tr className="text-center">
-              <th scope="col">Nome</th>
-              <th scope="col">Anteprima</th>
-              <th scope="col">Categorie</th>
-              <th scope="col">Disponibile</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts.map((post) => (
-              <tr key={post.id} className="text-center">
-                <th className="align-middle" scope="row">
-                  {post.title}
-                </th>
-                <td>
-                  <img src={apiUrl + post.image} width="150" />
-                </td>
-                <td className="align-middle">
-                  {post.categories.map((category) => (
-                    <span key={category} className="badge text-bg-info ms-2">
-                      {category}
-                    </span>
-                  ))}
-                </td>
-                <td className="align-middle">
-                  {post.published ? (
-                    <i className="fa-solid fa-check"></i>
-                  ) : (
-                    <i className="fa-solid fa-xmark"></i>
-                  )}
-                </td>
-                <td className="align-middle text-center">
-                  <Link to={`/posts/${post.id}`}>
-                    <i
-                      type="button"
-                      className="fa-solid fa-up-right-and-down-left-from-center"
-                    ></i>
-                  </Link>
-                  <i
-                    type="button"
-                    className="fa-solid fa-trash ms-4 text-danger"
-                  ></i>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className="container">
+      <h1 className="my-4">Posts</h1>
+      <PostsList />
+    </div>
   );
 }
