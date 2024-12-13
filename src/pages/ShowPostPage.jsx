a4ce9b00b8d7647c3678a4f8a304115d04d656e1
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-13 15:11:09 +0100

    add back button

--- src/pages/ShowPostPage.jsx
+++ src/pages/ShowPostPage.jsx
@@ -35,35 +35,45 @@ export default function ShowPostPage() {
   return (
     <div className="container">
       {post && (
-        <div className="card my-4">
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img
-                src={apiUrl + post.image}
-                className="img-fluid rounded-start"
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h2 className="card-title">{post.title}</h2>
-                <p className="card-text">{post.content}</p>
-                <p className="card-text fs-4">
-                  {post.published ? "Disponibile" : "Non Disponibile"}
-                </p>
-                <p className="card-text align-bottom">
-                  {post.categories.map((category) => (
-                    <span
-                      key={category}
-                      className="fs-5 badge text-bg-info ms-2"
-                    >
-                      {category}
-                    </span>
-                  ))}
-                </p>
+        <>
+          <div className="card my-4">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src={apiUrl + post.image}
+                  className="img-fluid rounded-start"
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h2 className="card-title">{post.title}</h2>
+                  <p className="card-text">{post.content}</p>
+                  <p className="card-text fs-4">
+                    {post.published ? "Disponibile" : "Non Disponibile"}
+                  </p>
+                  <p className="card-text align-bottom">
+                    {post.categories.map((category) => (
+                      <span
+                        key={category}
+                        className="fs-5 badge text-bg-info ms-2"
+                      >
+                        {category}
+                      </span>
+                    ))}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => navigate(-1)}
+          >
+            <i className="fa-solid fa-chevron-left me-2"></i>
+            Indietro
+          </button>
+        </>
       )}
     </div>
   );
