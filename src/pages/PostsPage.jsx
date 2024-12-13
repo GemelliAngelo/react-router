57b4cb017bd3afaca65221ac732bfcd0a67163ab
GemelliAngelo <gemeangelo27@gmail.com>
2024-12-13 15:50:31 +0100

    imports fix

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import PostsList from "../components/PostsList";
 
 export default function PostsPage() {
--- src/pages/ShowPostPage.jsx
+++ src/pages/ShowPostPage.jsx
@@ -1,38 +1,6 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 import PostCard from "../components/PostCard";
 
 export default function ShowPostPage() {
-  const navigate = useNavigate();
-  const postId = useParams().id;
-  const apiUrl = import.meta.env.VITE_API_URL;
-
-  const [post, setPost] = useState();
-
-  useEffect(() => {
-    fetchShowPost(postId);
-  }, []);
-
-  const fetchShowPost = (id) => {
-    const url = `${apiUrl}/posts/${id}`;
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          if (res.status == 404) {
-            navigate("not-found");
-          }
-          throw new Error("OPS! Qualcosa è andato storto");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setPost(data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
   return (
     <div className="container">
       <h1 className="my-4">DETTAGLIO POST</h1>
